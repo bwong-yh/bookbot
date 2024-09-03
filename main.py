af1e81685e558a0ca1609c4f089537bea5a3663f
Billy Wong <bwong.yh@gmail.com>
2024-09-02 18:23:09 -0600

    create function to generate report

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ def main():
     book_path = 'books/frankenstein.txt' 
 
     contents = read_book(book_path)
-    # print(contents)
-
     total_words = count_words(contents)
-    # print(total_words)
-
     word_dict = create_dict(contents)
-    # print(word_dict)
 
+    generate_report(book_path, total_words, word_dict)
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -27,6 +23,22 @@ def create_dict(text):
         lambda dict, word: {**dict, word.lower(): dict.get(word.lower(), 0) + 1}, 
         text.lower(),
         word_dict)
+
+def generate_report(book, word_count, word_dict):
+    print(f'--- Begin report of {book} ---')
+    print(f'{word_count} words found in the document\n')
+
+    dict_list = sorted(
+        [{k: v} for k,v in word_dict.items() if k.isalpha()], 
+        reverse=True, 
+        key=lambda dict: list(dict.values())[0]
+    )
+    for item in dict_list:
+        char = list(item.keys())[0]
+        count = list(item.values())[0]
+        print(f"The '{char}' character was found {count} times")
+
+    print('--- End report ---')
 
 if __name__ == '__main__':
     main()
